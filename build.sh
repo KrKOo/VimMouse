c39980b194fbe1f6e81e26867994a145667c80f7
KrKOo <krkoo.123@gmail.com>
2021-07-18 17:40:11 +0200

    FIX: build.sh replace return with exit

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ USAGE="Usage: build {linux|widows|mac}"
 
 
 if [[ -z "$1" || -n "$2" ]]; then
-	echo $USAGE 
-	return 1
+	echo "$USAGE" 
+	exit 1
 elif [[ "$1" == "linux" ]]; then
 	pip3 install -r requirements.txt &&
 	pyinstaller -F --clean --hidden-import "pynput.keyboard._xorg" --hidden-import "pynput.mouse._xorg" vimmouse.py
@@ -16,12 +16,12 @@ elif [[ "$1" == "mac" ]]; then
 	pip3 install -r requirements.txt &&
 	pyinstaller -F --clean --hidden-import "pynput.keyboard._darwin" --hidden-import "pynput.mouse._darwin" vimmouse.py
 else
-	echo $USAGE
-	return 1
+	echo "$USAGE"
+	exit 1
 fi
 
 echo "----------------------------------"
 echo "Your executable is ready in dist/"
 echo "----------------------------------"
 
-return 0
+exit 0
